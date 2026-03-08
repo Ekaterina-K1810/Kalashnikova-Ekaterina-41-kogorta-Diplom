6c0000f5744c3bde1eaad1c7fed20f72074e84b6
Ekaterina <kev1810@yandex.ru>
2026-03-08 20:20:24 +0300

    Загружено 2ое задание

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://8872b235-f2ab-4d05-960d-f2496ee2c4e8.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_TRACK_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+create_order = {
+    "firstName": "Роман",
+    "lastName": "Петров",
+    "address": "ул. Победы, 3, 6",
+    "metroStation": "4",
+    "phone": "+79111111111",
+    "rentTime": 5,
+    "deliveryDate": "2026-03-10",
+    "comment": "Привезите булочку",
+    "color": [
+        "BLACK"
+ ]
+}
